e4e4b901cec9956f5e9ec50a8f125104e741afff
pradeep <kumar031984@gmail.com>
2018-12-01 11:44:19 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ Route::group(['prefix' => 'en/v1/'], function (){
 	Route::any('login', 'Api\UserController@login')->name('login');
 	Route::any('gettoken', 'Api\ApiController@gettoken')->name('gettoken');
 	Route::any('register', 'Api\UserController@register')->name('register');
+	Route::any('getproductlist', 'Api\UserController@register')->name('getproductlist');
 });
